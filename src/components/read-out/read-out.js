cc39f5ea4fdcff157bb6b7731dca3cb7d5bb7801
Greg Lara <rglara@sep.com>
2018-03-25 11:03:08 -0400

    adding SASS support

--- src/components/read-out/read-out.js
+++ src/components/read-out/read-out.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TimeRow from '../time-row';
+import './read-out.scss';
 
 class ReadOut extends React.Component {
     render() {
         return (
-            <div>
+            <div className="read-out">
                 <TimeRow /> {/* Destination Time */}
                 <TimeRow /> {/* Present Time */}
                 <TimeRow /> {/* Last Time Departed */}
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
-    entry: './src/index.js',
+    entry: "./src/index.js",
     output: {
-        filename: 'bundle.js'
+        filename: "bundle.js",
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        }),
+    ],
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
-        ]
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.scss$/, use: [
+                MiniCssExtractPlugin.loader,
+                { loader: "css-loader" }, // translates CSS into CommonJS
+                { loader: "sass-loader" }, // compiles Sass to CSS
+            ]},
+        ],
     },
     devServer: {
-        contentBase: 'dist',
+        contentBase: "dist",
         compress: true,
-    }
+    },
 };
