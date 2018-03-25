d1c58188539d6f206c1415ac6b52ff5520840ada
Greg Lara <rglara@sep.com>
2018-03-25 09:38:40 -0400

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,9 @@ module.exports = {
         rules: [
             { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
         ]
+    },
+    devServer: {
+        contentBase: 'dist',
+        compress: true,
     }
 };
